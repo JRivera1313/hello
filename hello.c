c6de78d4ed5fb46149be5e8bf75b1797bfca4feb
JRivera1313 <46766747+JRivera1313@users.noreply.github.com>
2019-01-16 19:31:53 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* * Author: Jesus Rivera III
+ * Date: 2019/16/01
+ *
+ * A simple hello world program in C
+ *
+ */ 
+
+#include<stdio.h>
+
+int main(void) {
+
+	printf("Howdy World, Its me, ya boi!");
+
+   return 0;
+}
